856b592bdcad956b65111f27f0b188840cfdb2bc
saiteja059 <satlapallysaiteja110@gmail.com>
2025-03-11 09:03:38 +0530

    Add files via upload

---
+++ Exercise 2/Height_Category.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Height_Category
+{
+    public static void main(String args[])
+    {
+        int m;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the height:");
+        m = s.nextInt();
+        if(m > 175)
+        {
+            System.out.println("The person is tall.");
+        }
+        else if(m > 155 && m <= 175)
+        {
+            System.out.println("The person has average height.");
+        }
+        else
+        {
+            System.out.println("The person is dwarf.");
+        }
+    }
+}
